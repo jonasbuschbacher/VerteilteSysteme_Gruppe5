f716899965043b6bce530940ab025af10f834f27
Jonas Buschbacher <79661345+jonasbuschbacher@users.noreply.github.com>
2022-03-16 11:20:52 +0100

    Erste Übung

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import citiesRoutes from "./routes/cityRoutes.js";
+
+// complete application is here
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+
+//routes are /books & everything else throws a 404
+app.use("/cities", citiesRoutes);
+app.all("*", (req, res) => res.sendStatus(404));
+
+app.listen(port, () => {
+  console.log(`Server running on: http://localhost:${port}`);
+});
