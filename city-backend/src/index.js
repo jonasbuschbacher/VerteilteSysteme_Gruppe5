4ed9dba8c9ea33efe7786bd10aafd506b1fac777
Jonas Buschbacher <79661345+jonasbuschbacher@users.noreply.github.com>
2022-03-30 11:19:34 +0200

    mongoose, swagger express

--- city-backend/src/index.js
+++ city-backend/src/index.js
@@ -2,15 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import citiesRoutes from "./routes/cityRoutes.js";
 import mongoose from "mongoose";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocs from "../swaggerDocs.js";
 
 // complete application is here
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(bodyParser.json());
 
 //routes are /books & everything else throws a 404
 app.use("/cities", citiesRoutes);
+app.use("/api", swaggerUi.serve, swaggerUI.setup(swaggerDocs))
 app.all("*", (req, res) => res.sendStatus(404));
 
 mongoose.connect("mongodb://mongo:27017/test").then(() => {
